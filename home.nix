823479cefcad21c85fec0248d56baa39acd86a34
CEOZMaze <32793125+CEOZMaze@users.noreply.github.com>
2024-12-06 00:23:14 -0300

    Update home.nix

--- home.nix
+++ home.nix
@@ -1,34 +1,109 @@
+# /mnt/etc/nixos/home.nix
 
-{ config, pkgs, ... }: {
-  home.username = "alice";
-  home.homeDirectory = "/home/alice";
+{ config, pkgs, ... }:
+
+{
+  # User Information
+  home.username = "ceoz";
+  home.homeDirectory = "/home/ceoz";
+
+  # Programs Configuration
   programs = {
+    # Zsh Configuration
     zsh = {
       enable = true;
       shell = true;
       enableSyntaxHighlighting = true;
       enableAutosuggestions = true;
-      extraConfig = 'eval "$(starship init zsh)"';
+      extraConfig = ''
+        # Zsh Configuration
+        eval "$(starship init zsh)"
+        # Additional Zsh settings can go here
+      '';
     };
+
+    # Starship Configuration
     starship = {
       enable = true;
       config = "/etc/nixos/starship.toml";
     };
-    tmux = { enable = true; };
-    wezterm = { enable = true; };
+
+    # Tmux Configuration
+    tmux = {
+      enable = true;
+      # Tmux configuration is managed via ~/.tmux.conf
+    };
+
+    # WezTerm Configuration
+    wezterm = {
+      enable = true;
+      # Configuration handled via ~/.config/wezterm/wezterm.lua
+    };
+
+    # Neovim Configuration
     neovim = {
       enable = true;
       package = pkgs.neovim;
-      extraConfig = 'let g:have_nerd_font = 1';
+      extraConfig = ''
+        let g:have_nerd_font = 1
+      '';
     };
   };
-  home.packages = with pkgs; [ git make unzip gcc fzf exa bat ripgrep direnv nodejs go wezterm zsh tmux starship neovim ];
+
+  # Home Manager Packages
+  home.packages = with pkgs; [
+    # Essential Tools
+    git
+    make
+    unzip
+    gcc
+    fzf
+    exa
+    bat
+    ripgrep
+    direnv
+    nodejs
+    go
+    wezterm
+    zsh
+    tmux
+    starship
+    neovim
+  ];
+
+  # Home Files Configuration
   home.file = {
-    ".tmux.conf" = { text = 'set -g prefix C-a
-bind C-a send-prefix'; };
+    ".tmux.conf" = {
+      text = ''
+        # Tmux Configuration
+        set -g prefix C-a
+        bind C-a send-prefix
+        # Additional Tmux settings
+      '';
+    };
+
     ".config/wezterm/wezterm.lua" = {
-      text = 'local wezterm = require "wezterm"; return { font = wezterm.font("Fira Code"), font_size = 12.0, color_scheme = "Gruvbox Dark" }';
+      text = ''
+        -- WezTerm Configuration
+        local wezterm = require 'wezterm';
+        return {
+          font = wezterm.font("Fira Code"),
+          font_size = 12.0,
+          color_scheme = "Gruvbox Dark",
+          -- Add more settings as needed
+        }
+      '';
+    };
+
+    # Neovim Configuration using kickstart.nvim
+    ".config/nvim" = {
+      source = builtins.fetchGit {
+        url = "https://github.com/nvim-lua/kickstart.nvim.git";
+        rev = "master"; # or specify a commit hash for stability
+      };
     };
   };
+
+  # Enable direnv
   programs.direnv.enable = true;
 }
